869ba4c1e69f48c36544f2bba072bf29d70309e7
ariffebri22 <muhammadariffebriansyah@gmail.com>
2023-09-19 13:42:03 +0700

    Fixed(forgot): alt Image & Hooks

--- src/app/forgot/page.js
+++ src/app/forgot/page.js
@@ -6,7 +6,7 @@ import illustrasi2 from "../../../public/image/illustrationankasa.svg";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-export default function forgot() {
+export default function Forgot() {
     const [eye, setEye] = useState(false);
     const router = useRouter();
 
@@ -27,7 +27,7 @@ export default function forgot() {
         <>
             <div className="flex">
                 <div className="hidden lg:flex w-1/2 bg-main justify-center items-center">
-                    <Image src={illustrasi1} width={400} />
+                    <Image src={illustrasi1} width={400} alt="illustrasi" />
                 </div>
                 <div className=" w-screen h-screen lg:w-1/2 relative">
                     <div className="container  px-16 md:px-32 md:mx-auto">
@@ -36,10 +36,10 @@ export default function forgot() {
                             <h1 className="font-poppins font-bold text-abu text-lg ml-1">Ankasa</h1>
                         </div>
                         <div className="hidden md:flex justify-center md:mt-2 lg:hidden">
-                            <Image src={illustrasi2} width={400} />
+                            <Image src={illustrasi2} width={400} alt="illustrasi" />
                         </div>
                         <div className=" flex md:hidden justify-center mt-2 lg:hidden">
-                            <Image src={illustrasi2} width={200} />
+                            <Image src={illustrasi2} width={200} alt="illustrasi" />
                         </div>
                         <form className="mt-5 lg:mt-28" onSubmit={handleSubmit}>
                             <h1 className="font-poppins text-3xl font-bold">Forgot Password</h1>
