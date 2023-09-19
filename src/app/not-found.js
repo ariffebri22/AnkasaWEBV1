cd0a9a435c8d514b505faceb9612cab5b69a35f9
ariffebri22 <muhammadariffebriansyah@gmail.com>
2023-09-19 20:54:39 +0700

    Fixed(Error): Add Template Literals

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -22,7 +22,7 @@ export default function NotFound() {
                 </div>
                 <div className="flex justify-center items-center flex-col">
                     <h1 className="font-poppins bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-blue-400 to-main font-semibold text-[2rem] mt-0">Oops !</h1>
-                    <h1 className="font-poppins bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-blue-400 to-main font-semibold text-xl mt-0">It looks like you're lost.</h1>
+                    <h1 className="font-poppins bg-clip-text text-transparent bg-gradient-to-r from-blue-500 via-blue-400 to-main font-semibold text-xl mt-0">{`It looks like you're lost.`}</h1>
                 </div>
                 <div className="flex w-32 h-10  bg-main items-center justify-center rounded-md hover:bg-blue-600 cursor-pointer shadow-lg shadow-blue-500/50 mt-4" onClick={handleBack}>
                     <Link href="/" className="text-white text-sm font-medium">
